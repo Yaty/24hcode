b35edcd9ce4ab98f902ffa6503df9099def5d62e
Valentin <hamilcare@gmail.com>
2016-01-23 16:16:31 +0100

    Val07

--- case.py
+++ case.py
@@ -1,24 +1,46 @@
 from point import *
 from bloc import *
+import math
 
 
-def case:
+class case:
 	
-	def __init__(self,point,bloc)
-		self.typeCase= -1
-		self.x=int(point.getAbs/40)
-		self.y=int(point.getOrd/40)
+	def __init__(self):
+		self.typeCase=-1
+		self.x=0
+		self.y=0
+		
+	def setCase(self, bloc):
+		self.typeCase=bloc.type
+		self.x=int(bloc.point.getAbs() /40)
+		self.y=int(bloc.point.getOrd() /40)
+		
+	def caseLaser(self, perso):
+		self.type=-1
+		self.x=int(perso.get_x()/40)
+		self.y=int(perso.get_y()/40)
+		
+	def setCoord(self,x,y):
+		self.x=x
+		self.y=y
 		
 		
 	def __str__(self):
 		s=""
 		s+="Les coordonnees de la case sont :("
-		s+=str(self.x) + str(self.y) +") "
-		s+="Le type de la case est : "+str(self.typeCase)
+		s+=str(self.x) +","+ str(self.y) +") "
+		s+="Le type de la case est : "+str(self.typeCase) +"\n"
 		return s
 	
 	
-b = bloc
+"""b = bloc(200,200,1)
+
+c = case()
+
+c.setCase(b)
+
+
+print(c)"""
 		
 		
 	
--- laser.py
+++ laser.py
@@ -1,5 +1,6 @@
 from personnage import *
 #from game import *
+from case import *
 
 class laser:
 
@@ -15,18 +16,76 @@ class laser:
 			
 		return s
 		
-	def setDepart(self,pero):
+	def setDepart(self,perso):
 		self.liste=[]
-		self.ajouterPoint(perso.getPoint())
+		self.liste.append(self.getCase(perso))
+	
+	def getCase(self,perso):
+		c = case()
+		c.caseLaser(perso)
+		return c
+		
+	
+	def nextCase(self):
+		taille = len(self.liste)
+		c = case()
+		if(taille==1 and self.liste[0].x==0):
+			
+			c.setCoord(self.liste[0].x+1, self.liste[0].y)
+			
+		elif(taille==1 and self.liste[0].x==20):
+			
+			c.setCoord(self.liste[0].x-1, self.liste[0].y)
+		
+		elif(taille==1 and self.liste[0].y==0):
+			c.setCoord(self.liste[0].x, self.liste[0].y+1)
+			
+		elif(taille==1 and self.liste[0].y==15):
+			c.setCoord(self.liste[0].x, self.liste[0].y-1)
+			
+		else:
+			print("coucou ! \n")
+			tempo_x = self.liste[taille-1].x - self.liste[taille-2].x
+			tempo_y = self.liste[taille-1].y - self.liste[taille-2].y
+			
+			c.setCoord(self.liste[taille-1].x+tempo_x,self.liste[taille-1].y+tempo_y)
+			
 		
 			
-	def ajouterPoint(self,point):
-		self.liste.append(point)
+		return c
+		
+	def toucheBord(self):
+		taille = len(self.liste)
+		
+		if(self.liste[taille-1].x == 0 or self.liste[taille-1].x == 20  or self.liste[taille-1].y == 0  or self.liste[taille-1].y == 15):
+			return False
+		
+		return True 
+		
 	
 	
-	def tirer
-	
-	#def tirer(self,game):
+	def tirer(self,perso):
+		self.setDepart(perso)
+		self.liste.append(self.nextCase())
+		
+		continuer=True
+		
+		while(continuer):
+			self.liste.append(self.nextCase())
+			continuer = self.toucheBord()
 		
 		
+		
+perso = personnage(400,600)
+laz = laser()
+print("Laser avant tir : "+str(laz))
+
+
+laz.tirer(perso)
+
+
+print("Laser après tir : "+str(laz))
+
+
+
 	

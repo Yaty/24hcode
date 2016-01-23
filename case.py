08fe1809fe98ee9dd4328ed052fd2ae5d0afe353
Valentin <hamilcare@gmail.com>
2016-01-23 14:17:42 +0100

    val06

---
+++ case.py
@@ -0,0 +1,30 @@
+from point import *
+from bloc import *
+
+
+def case:
+	
+	def __init__(self,point,bloc)
+		self.typeCase= -1
+		self.x=int(point.getAbs/40)
+		self.y=int(point.getOrd/40)
+		
+		
+	def __str__(self):
+		s=""
+		s+="Les coordonnees de la case sont :("
+		s+=str(self.x) + str(self.y) +") "
+		s+="Le type de la case est : "+str(self.typeCase)
+		return s
+	
+	
+b = bloc
+		
+		
+	
+	
+		
+	
+		
+	
+		
